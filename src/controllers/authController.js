0a69ff660fbcadfda990941e119d1a58e537b4ee
Salim <salim.hassan.welleat@gmail.com>
2023-10-08 14:39:21 +0200

    refactor: userController errors handler

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,6 +12,10 @@ const tokenConfig = require("../config/token");
 
 const { promisify } = require("util");
 
+const catchAsync = require("../utils/catchAsync");
+
+const AppError = require("../utils/appError");
+
 const createToken = userId => {
   return jwt.sign({ id: userId }, tokenConfig.tokenJWT, { expiresIn: tokenConfig.tokenEXP });
 };
@@ -20,98 +24,75 @@ const correctPassword = async (candidatePassword, userPassword) => {
   return bcrypt.compare(candidatePassword, userPassword);
 };
 
-exports.login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
 
-    if (!validator.isEmail(email) || isEmpty(password)) {
-      return res.status(400).json({
-        status: "fail",
-        message: "Please provide email and password",
-      });
-    }
-
-    //Check if user exist and password is correct
-    const { rows: user } = await pool.query(`SELECT * FROM users WHERE email = $1;`, [email]);
-
-    const correct = await correctPassword(password, user[0].password);
-
-    if (!correct || isEmpty(user[0])) {
-      return res.status(401).json({
-        status: "fail",
-        message: "Incorrect email or password",
-      });
-    }
-
-    //If everything ok, send token to client
-    const token = createToken(user[0].id);
-
-    res.status(200).json({
-      message: "success",
-      token,
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "fail",
-      message: error,
-    });
+  if (!validator.isEmail(email) || isEmpty(password)) {
+    return next(new AppError("Please provide a valid email and password", 400));
   }
-};
 
-exports.signup = async (req, res) => {
-  try {
-    const { first_name, last_name, email, password } = req.body;
+  //Check if user exist and password is correct
+  const { rows: user } = await pool.query(`SELECT * FROM users WHERE email = $1;`, [email]);
 
-    if (
-      !validator.isEmail(email) ||
-      isEmpty(password) ||
-      isEmpty(first_name) ||
-      isEmpty(last_name)
-    ) {
-      return res.status(401).json({
-        status: "fail",
-        message: "User information error",
-      });
-    }
+  if (isEmpty(user)) {
+    return next(new AppError("No user found with that email", 404));
+  }
 
-    //Check if the email already used
-    const { rows: user } = await pool.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+  const correct = await correctPassword(password, user[0].password);
 
-    //If email already used
-    if (!isEmpty(user[0])) {
-      return res.status(401).json({
-        status: "fail",
-        message: "duplicate key value violates unique constraint users_email_key",
-      });
-    }
+  if (!correct || isEmpty(user[0])) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
 
-    //If not, create new user
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const { rows } = await pool.query(
-      `INSERT INTO users (first_name, last_name, email, password)
+  //If everything ok, send token to client
+  const token = createToken(user[0].id);
+
+  res.status(200).json({
+    message: "success",
+    token,
+  });
+});
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const { first_name, last_name, email, password } = req.body;
+
+  if (!validator.isEmail(email) || isEmpty(password) || isEmpty(first_name) || isEmpty(last_name)) {
+    return next(new AppError("Create user information error", 401));
+  }
+
+  //Check if the email already used
+  const { rows: user } = await pool.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+
+  //If email already used
+  if (!isEmpty(user[0])) {
+    return next(
+      new AppError("duplicate key value violates unique constraint users_email_key", 401),
+    );
+  }
+
+  //If not, create new user
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const { rows } = await pool.query(
+    `INSERT INTO users (first_name, last_name, email, password)
          VALUES 
             ($1, $2, $3, $4) RETURNING id, created_at,updated_at, first_name,photo,email ;`,
-      [first_name, last_name, email, hashedPassword],
-    );
+    [first_name, last_name, email, hashedPassword],
+  );
 
-    //Create token
-    const token = createToken(rows[0].id);
+  //Create token
+  const token = createToken(rows[0].id);
 
-    res.status(200).json({
-      message: "success",
-      data: {
-        token: token,
-        user: rows[0],
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "fail",
-      message: error,
-    });
-  }
-};
-exports.protect = async (req, res, next) => {
+  res.status(200).json({
+    message: "success",
+    data: {
+      token: token,
+      user: rows[0],
+    },
+  });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
   //1)Getting token and check of it's there
   let token;
 
@@ -120,22 +101,11 @@ exports.protect = async (req, res, next) => {
   }
 
   if (!token) {
-    return res.status(401).json({
-      status: "fail",
-      message: "You are not logged in! Please log in to get access.",
-    });
+    return next(new AppError("You are not logged in! Please log in to get access.", 401));
   }
   //2)Verification token
-  let decoded;
 
-  try {
-    decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  } catch (error) {
-    return res.status(401).json({
-      status: "fail",
-      message: "token invalid",
-    });
-  }
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   //3)Check if user still exist
   const { rows } = await pool.query(`SELECT * FROM users  WHERE id = $1;`, [decoded.id]);
@@ -143,14 +113,11 @@ exports.protect = async (req, res, next) => {
   const user = rows[0];
 
   if (isEmpty(user)) {
-    return res.status(401).json({
-      status: "fail",
-      message: "The user belonging to this token does no longer exist",
-    });
+    return next(new AppError("The user belonging to this token does no longer exist", 401));
   }
 
   //4) Check uf user changed password after the JWT was issued
   req.user = user;
 
   next();
-};
+});
