6b866bf1c919f5a1d1b534ead9b8fe56749f743b
Salim <salim.hassan.welleat@gmail.com>
2023-10-10 23:05:06 +0200

    build: error outside express, handling  unhandledRejections

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const pool = require("./src/config/pool");
 
 const port = appConfig.appPort || 3000;
 
+let server;
+
 pool
   .connect({
     host: dbConfig.dbHost,
@@ -17,7 +19,7 @@ pool
     password: "",
   })
   .then(() => {
-    app().listen(port, () => {
+    server = app().listen(port, () => {
       console.log("====================================");
       console.log(`Server running on port ${port}`);
       console.log("====================================");
@@ -26,3 +28,15 @@ pool
   .catch(err => {
     console.error(err);
   });
+
+//Centralized method to handle all unhandledRejections  in the application, by listing to unhandledRejections events
+process.on("unhandledRejection", err => {
+  console.log(err.name, err.message);
+
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  //Now we exit the application after unhandledRejections nicely be close the server then exit the application
+  server.close(() => {
+    //By running server close, we the server a time to finish all request that still pending or being handled at the time.
+    process.exit(1);
+  });
+});
