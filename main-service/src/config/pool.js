69a551f7f5b59c9922ade8acf0ea2c21907960c7
Salim <salim.hassan.welleat@gmail.com>
2023-11-15 19:23:22 +0100

    test: fix stopt point

--- main-service/src/config/pool.js
+++ main-service/src/config/pool.js
@@ -5,7 +5,7 @@ class Pool {
 
   connect(options) {
     this._pool = new pg.Pool(options);
-    console.log("Connected to PostgreSql with success");
+    console.log("Connected to PostgreSql with success 🧶🧶🧶");
     return this._pool.query("SELECT 1 + 1;");
   }
 
