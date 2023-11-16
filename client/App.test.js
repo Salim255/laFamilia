7a432e01a9a64388ff4cfdcbb815d3a9bb531cf0
Salim <salim.hassan.welleat@gmail.com>
2023-11-17 00:58:48 +0100

    build: setup react native test using jest and running first test

---
+++ client/App.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+it("render default text", () => {
+  let t = 1;
+  expect(t - 1).toEqual(0);
+});
---
+++ client/README.md
@@ -0,0 +1,5 @@
+- npm install --save-dev @testing-library/react-native
+- npx expo install jest-expo jest
+- "jest": {
+  "preset": "jest-expo"
+  },
