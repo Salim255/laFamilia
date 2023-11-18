6cb34f933a6a8f7ee7cb0b1fa9477d0d7481549c
Salim <salim.hassan.welleat@gmail.com>
2023-11-18 03:51:51 +0100

    build: setup client project test environement

--- client/App.test.js
+++ client/App.test.js
@@ -1,7 +1,16 @@
 import React from "react";
-import renderer from "react-test-renderer";
 
+import { screen, render } from "@testing-library/react-native";
+import App from "./App";
 it("render default text", () => {
-  let t = 1;
-  expect(t - 1).toEqual(0);
+  render(<App />);
+  //let t = 1;
+  // expect(t - 1).toEqual(0);
+  //const snap = renderer.create(<App />).toJSON();
+  //Test whether button exist or not, so 1st get the button
+  const btnElement = screen.getByRole("button", {
+    name: "Open up App.js to start working on your app!",
+    exact: false,
+  });
+  //Performe assertion
 });
---
+++ client/src/screens/SignUpScreen.js
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+function signUpScrren() {
+  return <View testID="sign-up-screen"></View>;
+}
+
+export default signUpScrren;
